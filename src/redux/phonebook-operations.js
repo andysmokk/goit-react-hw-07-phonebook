44ff5f152c0c6416a51d1e304e11bd68442eb333
Andrii Konovalov <78738364+andysmokk@users.noreply.github.com>
2021-11-13 11:47:19 +0100

    add rejectWithValue

--- src/redux/phonebook-operations.js
+++ src/redux/phonebook-operations.js
@@ -12,21 +12,39 @@ import axios from 'axios';
 //   // deleteContactError,
 // } from './phonebook-actions';
 
-export const postAddContact = createAsyncThunk('addContacts', async contact => {
-  await axios.post('http://localhost:3000/contacts', contact);
-  return contact;
-});
+export const postAddContact = createAsyncThunk(
+  'addContacts',
+  async (contact, { rejectWithValue }) => {
+    try {
+      await axios.post('http://localhost:3000/contacts', contact);
+      return contact;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
 
-export const fetchContacts = createAsyncThunk('fetchContact', async () => {
-  const { data } = await axios.get('http://localhost:3000/contacts');
-  return data;
-});
+export const fetchContacts = createAsyncThunk(
+  'fetchContact',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.get('http://localhost:3000/contacts');
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+);
 
 export const deleteContact = createAsyncThunk(
   'deleteContact',
-  async contactId => {
-    await axios.delete(`http://localhost:3000/contacts/${contactId}`);
-    return contactId;
+  async (contactId, { rejectWithValue }) => {
+    try {
+      await axios.delete(`http://localhost:3000/contacts/${contactId}`);
+      return contactId;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
   },
 );
 
